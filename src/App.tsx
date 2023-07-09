395185ddc279cf23bd901a46f41998b77e85bcc4
Dominika <werkamdor@gmail.com>
2023-07-09 17:29:35 +0200

    Main page view prepared

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import {
+  Paper,
+  TextInput,
+  Button,
   ColorScheme,
   ColorSchemeProvider,
   Group,
@@ -7,12 +10,13 @@ import {
   Title,
   createStyles,
   rem,
+  Text,
 } from "@mantine/core";
 import ColorThemeButton from "./components/ColorThemeButton";
 import { useColorScheme, useHotkeys } from "@mantine/hooks";
 import { useState } from "react";
-import { IconPigMoney } from '@tabler/icons-react';
-import styled, { keyframes } from 'styled-components';
+import { IconPigMoney } from "@tabler/icons-react";
+import styled, { keyframes } from "styled-components";
 
 const pulseAnimation = keyframes`
   0% {
@@ -36,16 +40,41 @@ const StyledPigIcon = styled(IconPigMoney)`
 export default function App() {
   const useStyles = createStyles((theme) => ({
     header: {
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
       padding: `${rem(8)} ${rem(12)}`,
+    },
+    wrapper: {
+      height: "calc(100vh - 60px)",
+      backgroundSize: "75% auto",
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "right",
+      backgroundImage:
+        "url(https://images.unsplash.com/photo-1538356343135-65849f66b4a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=873&q=80)",
+    },
+
+    form: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      height: "calc(100vh - 60px)",
+      borderRight: `${rem(1)} solid ${
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[7]
+          : theme.colors.gray[3]
+      }`,
+      maxWidth: rem(500),
+      [theme.fn.smallerThan("sm")]: {
+        maxWidth: "100%",
+      },
     },
   }));
 
   const { classes } = useStyles();
   const preferredColorScheme = useColorScheme();
-  const [colorScheme, setColorScheme] = useState<ColorScheme>(preferredColorScheme);
+  const [colorScheme, setColorScheme] =
+    useState<ColorScheme>(preferredColorScheme);
 
   const toggleColorScheme = (value?: ColorScheme) =>
     setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
@@ -64,11 +93,37 @@ export default function App() {
       >
         <Header height={60} className={classes.header}>
           <Group>
-          <StyledPigIcon size="2rem" strokeWidth="1.25" />
-          <Title order={3}>Moje wydatki</Title>
+            <StyledPigIcon size="2rem" strokeWidth="1.25" />
+            <Title order={3}>Moje wydatki</Title>
           </Group>
           <ColorThemeButton />
         </Header>
+        <div className={classes.wrapper}>
+          <Paper className={classes.form} radius={0} p={30}>
+            <Title order={2} ta="center" mt="md" mb={50} style={{marginTop:'-25%'}}>
+              Zacznij śledzić swój stan konta!
+            </Title>
+
+            <TextInput label="Jak masz na imię?" placeholder="Ania" size="md" />
+            <TextInput
+              label="Od jakiej kwoty zaczynamy śledzenie?"
+              placeholder="500"
+              mt="md"
+              size="md"
+            />
+            <Button
+              fullWidth
+              mt="xl"
+              size="md"
+              variant="gradient"
+              gradient={{ from: "teal", to: "lime", deg: 105 }}
+              radius="xl"
+            >
+              Zaczynamy
+            </Button>
+            <Text fz="xs" ta="center" mt="md" mb={50} c="dimmed">Twoje dane będą dostępne tylko dla Ciebie na Twoim komputerze</Text>
+          </Paper>
+        </div>
       </MantineProvider>
     </ColorSchemeProvider>
   );
